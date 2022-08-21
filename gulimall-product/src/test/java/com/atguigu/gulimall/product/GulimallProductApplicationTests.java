b72872d5eac59ebe1d3551d435a3d350823d142d
WIN-RT09V4GCT0I\Administrator <1679569188@qq.com>
2022-08-21 16:12:59 +0800

    实现oss存储测试

--- gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
+++ gulimall-product/src/test/java/com/atguigu/gulimall/product/GulimallProductApplicationTests.java
@@ -1,5 +1,9 @@
 package com.atguigu.gulimall.product;
 
+import com.aliyun.oss.ClientException;
+import com.aliyun.oss.OSS;
+import com.aliyun.oss.OSSClientBuilder;
+import com.aliyun.oss.OSSException;
 import com.atguigu.gulimall.product.entity.BrandEntity;
 import com.atguigu.gulimall.product.entity.CategoryEntity;
 import com.atguigu.gulimall.product.service.BrandService;
@@ -14,7 +18,10 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
@@ -35,8 +42,54 @@ public class GulimallProductApplicationTests {
     BrandService brandService;
 
 
+
+
     @Autowired
     CategoryService categoryService;
+
+
+
+    @Test
+    public void uploadFIle(){
+        String endpoint = "oss-cn-shenzhen.aliyuncs.com";
+        // 阿里云账号AccessKey拥有所有API的访问权限，风险很高。强烈建议您创建并使用RAM用户进行API访问或日常运维，请登录RAM控制台创建RAM用户。
+        String accessKeyId = "LTAI5t6H1AQEpUSRBCk5WWPP";
+        String accessKeySecret = "5um65q9wPFu9BLq50A4UIkmXVX3phC";
+        // 填写Bucket名称，例如examplebucket。
+        String bucketName = "shopweb123";
+        // 填写Object完整路径，完整路径中不能包含Bucket名称，例如exampledir/exampleobject.txt。
+        String objectName = "t1.img";
+        // 填写网络流地址。
+        String url = "https://i0.hdslb.com/bfs/banner/bb4260fd7f74a41b1d39e8510795ba91b5fb970f.jpg@1200w_300h_1c.webp";
+
+        // 创建OSSClient实例。
+        OSS ossClient = new OSSClientBuilder().build(endpoint, accessKeyId, accessKeySecret);
+
+        try {
+            InputStream inputStream = new URL(url).openStream();
+            // 创建PutObject请求。
+            ossClient.putObject(bucketName, objectName, inputStream);
+        } catch (OSSException oe) {
+            System.out.println("Caught an OSSException, which means your request made it to OSS, "
+                    + "but was rejected with an error response for some reason.");
+            System.out.println("Error Message:" + oe.getErrorMessage());
+            System.out.println("Error Code:" + oe.getErrorCode());
+            System.out.println("Request ID:" + oe.getRequestId());
+            System.out.println("Host ID:" + oe.getHostId());
+        } catch (ClientException | MalformedURLException ce) {
+            System.out.println("Caught an ClientException, which means the client encountered "
+                    + "a serious internal problem while trying to communicate with OSS, "
+                    + "such as not being able to access the network.");
+            System.out.println("Error Message:" + ce.getMessage());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            if (ossClient != null) {
+                ossClient.shutdown();
+            }
+        }
+    }
+
 
     @Test
     public void testFindPath(){
---
+++ gulimall-search/src/main/java/com/atguigu/gulimall/search/service/ProductSaveService.java
@@ -0,0 +1,19 @@
+package com.atguigu.gulimall.search.service;
+
+
+
+import com.atguigu.gulimall.search.common.SkuEsModel;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * @Description:
+ * @Created: with IntelliJ IDEA.
+ * @author: 夏沫止水
+ * @createTime: 2020-06-06 16:53
+ **/
+public interface ProductSaveService {
+
+    boolean productStatusUp(List<SkuEsModel> skuEsModels) throws IOException;
+}
